4343280dbd133dababab4c17de305fe5df66e624
peter precious <peteprecious370@gmail.com>
2024-06-26 11:26:18 -0700

    fixed

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 '''a module to perform lifo'''
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
     '''a class for lifo'''
     def __init__(self):
-        """ Initialize the class """
+        """Initializes the cache.
+        """
         super().__init__()
-        self.order = []  # List to keep track of the order of keys
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                # Remove the key if it already exists to update its
-                self.order.remove(key)
-            self.cache_data[key] = item
-            self.order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # Remove the last item added (LIFO)
-                last_key = self.order.pop()
-                del self.cache_data[last_key]
-                print(f"DISCARD: {last_key}")
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """ Get an item by key """
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
